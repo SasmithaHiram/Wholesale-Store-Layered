0164cd21f3676febb63dfea7607549f45b566c1a
SasmithaHiram <sasmithahiram2003@gmail.com>
2025-02-11 18:50:45 +0530

    Hibernate Completed

--- src/main/java/repository/custom/impl/CustomerDaoImpl.java
+++ src/main/java/repository/custom/impl/CustomerDaoImpl.java
@@ -1,7 +1,9 @@
 package repository.custom.impl;
 
+import config.HibernateConfig;
 import dto.Customer;
 import entity.CustomerEntity;
+import org.hibernate.Session;
 import repository.custom.CustomerDao;
 
 import java.util.List;
@@ -11,7 +13,14 @@ public class CustomerDaoImpl implements CustomerDao {
     @Override
     public boolean save(CustomerEntity entity) {
         System.out.println(entity);
-        return false;
+        Session session = HibernateConfig.getSession();
+        session.beginTransaction();
+        session.persist(entity);
+        session.getTransaction().commit();
+        session.close();
+
+        return true;
+
     }
 
     @Override
